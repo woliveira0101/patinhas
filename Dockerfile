c17ee3a06c84a5a0de5da5a8994c8f6d079ea8ad
Wellington Oliveira <woliveira0101@gmail.com>
2024-05-26 06:40:25 -0300

    Projeto alterado para não utilizar o autoload do Composer

--- Dockerfile
+++ Dockerfile
@@ -9,24 +9,7 @@ RUN apt-get update && apt-get upgrade -y \
 # Instalar extensões PHP necessárias
 RUN docker-php-ext-install -j$(nproc) mysqli pdo_mysql gd zip pcntl exif intl bcmath
 
-# allow super user - set this if you use Composer as a
-# super user at all times like in docker containers
-ENV COMPOSER_ALLOW_SUPERUSER=1
-
-# obtain composer using multi-stage build
-# https://docs.docker.com/build/building/multi-stage/
-COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
-
-# Here, we are copying only composer json and composer. lock (instead of copying the entire source
-# right before doing composer install.
-# This is enough to take advantage of docker cache and composer install will
-# be executed only when composer json or composer. lock have indeed changed! -
-# https://medium.com/@softius/faster-docker-builds-with-composer-install-b4d2b15d0fff
-COPY ./composer.* ./
-
 EXPOSE 80
-# install
-RUN composer install --prefer-dist --no-dev --no-scripts --no-progress --no-interaction
 
 # Habilitar mod_rewrite para o Apache
 RUN a2enmod headers expires rewrite
@@ -45,9 +28,6 @@ RUN sed -i '/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/Allo
 
 # Copiar o conteúdo do projeto para o container
 COPY . /var/www/html/
-
-# run composer dump-autoload --optimize
-RUN composer dump-autoload --optimize
 
 # Ajustar as permissões dos arquivos
 RUN chown -R www-data:www-data /var/www/html
--- app/routes/routes.php
+++ app/routes/routes.php
@@ -2,16 +2,37 @@
 
 session_start();
 
-require __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../config/Database.php';
 
+// Include all models
+require_once __DIR__ . '/../models/Model.php';
+require_once __DIR__ . '/../models/AdoptionModel.php';
+require_once __DIR__ . '/../models/PetImageModel.php';
+require_once __DIR__ . '/../models/AddressModel.php';
+require_once __DIR__ . '/../models/PetModel.php';
+require_once __DIR__ . '/../models/UserModel.php';
+require_once __DIR__ . '/../models/DonationModel.php';
+require_once __DIR__ . '/../models/QuestionModel.php';
+
+// Include all controllers
+require_once __DIR__ . '/../controllers/Controller.php';
+require_once __DIR__ . '/../controllers/HomeController.php';
+require_once __DIR__ . '/../controllers/UserController.php';
+require_once __DIR__ . '/../controllers/AddressController.php';
+require_once __DIR__ . '/../controllers/AdminController.php';
+require_once __DIR__ . '/../controllers/PetController.php';
+require_once __DIR__ . '/../controllers/PetImageController.php';
+require_once __DIR__ . '/../controllers/DonationController.php';
+require_once __DIR__ . '/../controllers/AdoptionController.php';
+
+// Namespace aliases
+use App\Controllers\HomeController;
+use App\Controllers\UserController;
 use App\Controllers\AddressController;
 use App\Controllers\AdminController;
-use App\Controllers\AdoptionController;
-use App\Controllers\DonationController;
-use App\Controllers\HomeController;
 use App\Controllers\PetController;
-use App\Controllers\PetImageController;
-use App\Controllers\UserController;
+use App\Controllers\DonationController;
+use App\Controllers\AdoptionController;
 
 $uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 $controller = 'HomeController';
@@ -20,32 +41,30 @@ $id = null;
 $parts = explode('/', $uri);
 
 if (!empty($parts[0]) && !empty($parts[1])) {
-    $controller = 'App\\Controllers\\' . ucfirst(strtolower($parts[0])) . 'Controller';
+    $controller = ucfirst(strtolower($parts[0])) . 'Controller';
     $action = strtolower($parts[1]);
     if (!empty($parts[2])) {
         $id = intval($parts[2]);
     }
 } elseif ($uri === '') {
-    $controller = 'App\\Controllers\\HomeController';
+    $controller = 'HomeController';
     $action = 'index';
 } elseif ($uri == 'user/register') {
-    $controller = 'App\\Controllers\\UserController';
+    $controller = 'UserController';
     $action = 'register';
 } elseif ($uri == 'pets') {
-    $controller = 'App\\Controllers\\PetController';
+    $controller = 'PetController';
     $action = 'index';
 }
 
-if (class_exists($controller) && method_exists($controller, $action)) {
-    if ($controller !== 'App\\Controllers\\HomeController' && !isset($_SESSION['user_id']) && !in_array($uri, ['user/login', 'user/authenticate', 'user/register'])) {
+if (class_exists("App\\Controllers\\$controller") && method_exists("App\\Controllers\\$controller", $action)) {
+    if ($controller !== 'HomeController' && !isset($_SESSION['user_id']) && !in_array($uri, ['user/login', 'user/authenticate', 'user/register'])) {
         header('Location: /');
         exit();
     }
 
-    $controllerInstance = new $controller();
-    // Log para depuração
-    // print_r("Controller: $controller, Action: $action, ID: " . ($id ?? 'null'));
-
+    $controllerClass = "App\\Controllers\\$controller";
+    $controllerInstance = new $controllerClass();
     if (in_array($action, ['edit', 'show', 'delete', 'update', 'request', 'cancel', 'showrequests']) && !is_null($id)) {
         $controllerInstance->$action($id);
     } elseif ($_SERVER['REQUEST_METHOD'] == 'GET' && in_array($action, ['index', 'create', 'login', 'register', 'dashboard', 'profile', 'mydonations', 'myadoptions', 'address', 'list', 'registration', 'request', 'success'])) {
